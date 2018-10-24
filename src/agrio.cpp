3f7f7b107c5d14fa5e56860ecba0cd85dace647e
Vasilii Kuznetsov <vasilii.kuznetsov@aggregion.com>
2018-10-24 12:29:35 +0300

    AGR-22. Populate agr term.

--- src/agrio.cpp
+++ src/agrio.cpp
@@ -1,10 +1,10 @@
 #pragma once
 
 /**
- * Define specific things for the eosio system
+ * Define specific things for the agrio system
  */
 
 extern "C" {
-   void eosio_assert(unsigned int, const char*);
-   void __cxa_pure_virtual() { eosio_assert(false, "pure virtual method called"); }
+   void agrio_assert(unsigned int, const char*);
+   void __cxa_pure_virtual() { agrio_assert(false, "pure virtual method called"); }
 }
